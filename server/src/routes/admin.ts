a046cb6c3a07fbf10833c2ebd10b91d8c35b71e5
JU <151619236+jux03x@users.noreply.github.com>
2026-08-04 15:34:23 +0200

    Update admin.ts

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -50,7 +50,7 @@ adminRouter.get('/images', async (req: AuthRequest, res: Response) => {
 
   const total = (await db.query('SELECT COUNT(*)::int FROM images WHERE is_deleted=FALSE')).rows[0].count;
 
-  const images = await Promise.all(result.rows.map(addUrls));
+  const images = await Promise.all(rows.rows.map(addUrls));
   // const images = await Promise.all(rows.rows.map(async img => ({
   //   ...img,
   //   url: await getSignedUrl(img.storage_path).catch(() => ''),
